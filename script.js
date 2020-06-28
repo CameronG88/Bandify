d28a0158de73f9b8bfcaaa8b216872e3e75126f8
Cameron Graham <cameron.g882@gmail.com>
2020-06-28 18:58:22 -0400

    got a lot of the js for album searches complete

--- script.js
+++ script.js
@@ -80,16 +80,29 @@ $("#searchBtn").on("click", function (e) {
             url: deezerURL,
             method: "GET"
         }).then(function (response) {
-            // for (let index = 0; index < response.data.length; index++) {
-            //     var newLi = $("<li>").text(response.data[index].title);
-            //     newLi.addClass("songId");
-            //     newLi.data("songId", response.data[index].id)
-            //     // Prepends song list created to Deezer song list container
-            //     $("#songsList").prepend(newLi);
-            // }
-            console.log(response)
-        });
+            console.log(response);
+            var albumArtist = $("<h1>").text(response.data[0].title + " by: " + response.data[0].artist.name);
+            albumArtist.addClass("artTitle");
+            var albumImg = $("<img>").attr("src", response.data[0].cover_medium);
+            albumImg.css({ "width": "200", "height": "200", "border-radius": "10px 0 0 10px", "float": "left" });
+            $("#bandsList").prepend(albumImg);
+            $("#titleDiv").append(albumArtist);
 
+            var albumListURL = "https://cors-anywhere.herokuapp.com/api.deezer.com/album/" + response.data[0].id + "/tracks"
+            $.ajax({
+                url: albumListURL,
+                method: "GET"
+            }).then(function (response) {
+                for (let index = 0; index < response.data.length; index++) {
+                    var newLi = $("<li>").text(response.data[index].title);
+                    newLi.addClass("songId");
+                    newLi.data("songId", response.data[index].id)
+                    // Prepends song list created to Deezer song list container
+                    $("#songsList").prepend(newLi);
+                }
+                console.log(response)
+            });
+        })
 
         // var queryURL = "https://cors-anywhere.herokuapp.com/rest.bandsintown.com/artists/" + search + "?app_id=codingbootcamp";
         // $.ajax({
@@ -134,6 +147,7 @@ $("#searchBtn").on("click", function (e) {
         // })
 
         // console.log(searchCrit);
+
     }
 });
 // Call to Deezer API to display song player on page
